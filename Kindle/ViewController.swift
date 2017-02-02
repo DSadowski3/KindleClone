ad810a35bf1d3a831a6f37a2a77e033dcdd5d34f
DSadowski3 <dsadowski2001@dominiks-macbook-pro.local>
2017-02-02 13:06:17 -0600

    Initial Commit

---
+++ Kindle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kindle
+//
+//  Created by Dominik Sadowski on 2/2/17.
+//  Copyright © 2017 Dominik Sadowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
