266a13408e7ab33b6a00ee5683f6185c3fa077eb
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2017-02-02 15:46:11 -0600

    Added imageview and labels to the tableviewcell

--- Kindle/BookCell.swift
+++ Kindle/BookCell.swift
@@ -9,8 +9,36 @@
 import UIKit
 
 class BookCell: UITableViewCell {
+    
+    let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is the text"
+        return label
+    }()
+    
+    let authorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This authors name"
+        return label
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(coverImageView)
+        addSubview(titleLabel)
+        addSubview(authorLabel)
+        
+        coverImageView.frame = CGRect(x: 8, y: 8, width: 50, height: 64)
+        titleLabel.frame = CGRect(x: 66, y: 20, width: 200, height: 20)
+        authorLabel.frame = CGRect(x: 66, y: 48, width: 200, height: 20)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
