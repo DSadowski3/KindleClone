0158563c0fbf95c96d760bcc6acd97c849596655
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2017-02-03 14:11:51 -0600

    Collectionview Cell is reciving correct data as well as allows user to return back to the list of books

--- Kindle/BookPagerController.swift
+++ Kindle/BookPagerController.swift
@@ -10,28 +10,36 @@ import UIKit
 
 class BookPagerController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    var book: Book?
+    
     let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .white
-        navigationItem.title = "Book"
-        
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        navigationItem.title = self.book?.title
+       
+        collectionView?.register(PageCell.self, forCellWithReuseIdentifier: cellId)
         
         let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout
         layout?.scrollDirection = .horizontal
         layout?.minimumLineSpacing = 0
         collectionView?.isPagingEnabled = true
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(handleCloseBook))
+    }
+    
+    func handleCloseBook() {
+        dismiss(animated: true, completion: nil)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
+        return CGSize(width: view.frame.width, height: view.frame.height-44-20)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return book?.pages.count ?? 0
     }
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -39,13 +47,11 @@ class BookPagerController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let pageCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PageCell
         
-        if indexPath.item % 2 == 0 {
-            cell.backgroundColor = .red
-        }else {
-            cell.backgroundColor = .blue
-        }
-        return cell
+        let page = book?.pages[indexPath.item]
+        pageCell.textLabel.text = page?.text
+        
+        return pageCell
     }
 }
